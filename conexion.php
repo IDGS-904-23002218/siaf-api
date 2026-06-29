1e9a024e2a1663ebf4641fa8876bf8c6300dc4b7
Juan Carlos López <cr1757579@gmail.com>
2026-06-28 22:18:13 -0600

    SIAF-3

--- conexion.php
+++ conexion.php
@@ -1,16 +1,17 @@
 <?php
+error_reporting(0);
+ini_set('display_errors', 0);
+
 $host     = "sql105.infinityfree.com";
 $db       = "if0_42292004_siaf";
 $usuario  = "if0_42292004";
 $password = "8fIRgNGCUz";
 
 $conexion = new mysqli($host, $usuario, $password, $db);
-
 if ($conexion->connect_error) {
     http_response_code(500);
     echo json_encode(["error" => "Error de conexión: " . $conexion->connect_error]);
     exit();
 }
-
 $conexion->set_charset("utf8");
 ?>
