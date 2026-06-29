d4f799ce77a2b8f4e88cc09e61d00a702d4bb31e
IDGS-904-23002218 <cr1757579@gmail.com>
2026-06-28 22:29:49 -0600

    Update database connection to use environment variables

--- conexion.php
+++ conexion.php
@@ -2,12 +2,13 @@
 error_reporting(0);
 ini_set('display_errors', 0);
 
-$host     = "sql105.infinityfree.com";
-$db       = "if0_42292004_siaf";
-$usuario  = "if0_42292004";
-$password = "8fIRgNGCUz";
+$host     = getenv("MYSQLHOST");
+$db       = getenv("MYSQLDATABASE");
+$usuario  = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$puerto   = (int)(getenv("MYSQLPORT") ?: 3306);
 
-$conexion = new mysqli($host, $usuario, $password, $db);
+$conexion = new mysqli($host, $usuario, $password, $db, $puerto);
 if ($conexion->connect_error) {
     http_response_code(500);
     echo json_encode(["error" => "Error de conexión: " . $conexion->connect_error]);
